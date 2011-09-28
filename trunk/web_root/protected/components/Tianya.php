785e1391bc9ce76ad3113662d7e4532836042e37
xami520@qq.com <xami520@qq.com@496018d3-3f67-c9eb-2849-61d8cf1ab3fe>
2011-09-28 08:24:19 +0000

    z

---
+++ trunk/web_root/protected/components/Tianya.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lijia
+ * Date: 11-9-28
+ * Time: 下午1:43
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+class Tianya{
+    function getChannels(){
+        $data['channels']=Channel::model()->with('items')->findAll('t.status=1');
+
+        $this->render('channels', $data);
+    }
+
+    function getChannel($id){
+        $id=intval($id);
+        $data['channel']=Channel::model()->with('items')->find('t.status=1 AND t.id=:id',array(':id'=>$id));
+
+        $this->render('channel', $data);
+    }
+
+    function getItems(){
+        $data['items']=Item::model()->findAll('`status`=1');
+
+        $this->render('channel', $data);
+    }
+
+    function getItem($id){
+        $id=intval($id);
+        $data['item']=Item::model()->find('`id`='.$id.' AND `status`=1');
+
+        $this->render('channel', $data);
+    }
+
+    function getArticles(){
+        $data['Articles']=Article::model()->findAll('`status`=1');
+
+        $this->render('channel', $data);
+    }
+
+    function getArticle($id){
+        $id=intval($id);
+        $data['Article']=Article::model()->find('`id`='.$id.' AND `status`=1');
+
+        $this->render('channel', $data);
+    }
+
+    function setArticle(){
+        
+    }
+
+    function setList(){
+        
+    }
+
+    function setChannel(){
+        
+    }
+}
