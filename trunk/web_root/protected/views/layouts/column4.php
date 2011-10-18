61ce530d58b20f457ef13ad4b99d0f14516a95df
xami520@qq.com <xami520@qq.com@496018d3-3f67-c9eb-2849-61d8cf1ab3fe>
2011-10-18 09:56:53 +0000

    x

--- trunk/web_root/protected/views/layouts/column4.php
+++ trunk/web_root/protected/views/layouts/column4.php
@@ -7,30 +7,6 @@
 	</div>
 	<div class="span-5 last">
 		<div id="sidebar">
-		<?php
-			/*$this->beginWidget('zii.widgets.CPortlet', array(
-				'title'=>'Operations',
-			));
-			$this->widget('zii.widgets.CMenu', array(
-				'items'=>$this->menu,
-				'htmlOptions'=>array('class'=>'operations'),
-			));
-			$this->endWidget();*/
-		?>
-<script type="text/javascript"><!--
-google_ad_client = "pub-4726192443658314";
-/* 160x600, 创建于 10-5-2 */
-google_ad_slot = "6942291543";
-google_ad_width = 160;
-google_ad_height = 600;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-<div style="clear:both;"></div>
-
-
 		</div><!-- sidebar -->
 	</div>
 </div>
